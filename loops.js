4815ea938998d13353838fc1052e92ea009b59f8
satyasinha94 <satyasinha94@gmail.com>
2018-08-31 20:59:07 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -17,13 +17,11 @@ function whileLoop(n) {
   }
   }
 }
-var i = 0;
-function incrementVariable() {
-  i = i + 1;
-}
 function doWhileLoop(array) {
   do {
     array.pop ();
   }
   while (array.length > 0)
+}
+return array;
 }
